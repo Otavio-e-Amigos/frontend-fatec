133c9f4f434f3bab6f6e3267c5228997af01efbd
DiegoG784 <diegofaria865@gmail.com>
2026-09-10 00:22:05 -0300

    initial professor class implementation

---
+++ app/modules/professor/professor.class.ts
@@ -0,0 +1,15 @@
+export default class Professor {
+	private name: string
+	private registry: string
+	private course: string
+	private courseClass: number
+
+	constructor(name: string, registry: string, course: string, courseClass: number) {
+		this.name = name
+		this.registry = registry
+		this.course = course
+		this.courseClass = courseClass
+	}
+
+	// TODO GETTER and SETTER here
+}
