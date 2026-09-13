7c919db1dc9e74c1a78a19bf543cb2e7158e44e1
DiegoG784 <diegofaria865@gmail.com>
2026-09-13 13:23:25 -0300

    experimental header styling

--- app/layouts/Header.tsx
+++ app/layouts/Header.tsx
@@ -1,24 +1,36 @@
 import { Link } from "react-router"
 
 const links = [
-  {label: "Página inicial", href: "/"},
-  {label: "Grade", href: "/"},
-  {label: "Professores", href: "/", items: [
+  // {label: "Página inicial", href: "/"},
+  {label: "Grades", href: "/"},
+  {label: "Professores", href: "/professor", items: [
     {label: "Gerenciar Professores"},
     {label: "Gerenciar Cursos"},
   ]},
 ]
 
-export default function Header() {
-  return (
-    <header className="flex flex-col gap-3">
-      <section>
+function HeaderLink({label, href, active}: {label:string, href:string, active?:boolean}) {
+	return <Link to={href} className={`py-1 px-2 rounded-lg ${active ? "bg-indigo-500 hover:bg-indigo-700" : "bg-indigo-500/20 hover:bg-indigo-500/50"}`}>{label}</Link>
+}
 
-      </section>
-      <nav className="flex flex-row gap-2">
-				{process.env.NODE_ENV === 'development' && <Link to={"/dev-area"}>dev-area</Link>}
-				{links.map(({ label, href }, idx) => { return <Link key={idx} to={href}>{label}</Link> })}
-      </nav>
-    </header>
+export default function Header({activeItem}: {activeItem?: string}) {
+	return (
+		<>
+			<header className="header flex flex-col px-3 pb-2">
+				<div className="flex flex-row gap-10 items-center">
+
+					<div className="flex flex-row gap-2 items-center">
+						<img src="/favicon.ico" className="size-10"/>
+						<Link to={"/"}>Appointer</Link>
+					</div>
+
+		      <nav className="flex flex-row gap-2 items-center">
+						{links.map(({ label, href }, idx) => <HeaderLink key={idx} label={label} href={href} active={label === activeItem ? true : false} /> )}
+						{process.env.NODE_ENV === 'development' && <Link to={"/dev-area"} className="py-1 px-2 rounded-lg bg-rose-900 hover:bg-rose-950">dev-area</Link>}
+		      </nav>
+				</div>
+	    </header>
+			<div className="min-h-3 bg-gray-800 w-42 -skew-x-25 -translate-x-4"/>
+		</>
   )
 }
