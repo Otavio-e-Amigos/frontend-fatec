7bbcd0477283d2ae8a03cc5fb4914b3ec3d47d85
DiegoG784 <diegofaria865@gmail.com>
2026-09-13 13:25:30 -0300

    Update professor.class.ts

--- app/modules/professor/professor.class.ts
+++ app/modules/professor/professor.class.ts
@@ -1,14 +1,16 @@
 export default class Professor {
-	private name: string
-	private registry: string
-	private course: string
-	private courseClass: number
+	name: string
+	registry: string
+	govID: string
+	courses: string[]
+	courseClass: number
 
-	constructor(name: string, registry: string, course: string, courseClass: number) {
+	constructor(name: string, govID: string, registry: string, courses: string[], courseClass: number) {
 		this.name = name
 		this.registry = registry
-		this.course = course
+		this.courses = courses
 		this.courseClass = courseClass
+		this.govID = govID
 	}
 
 	// TODO GETTER and SETTER here
