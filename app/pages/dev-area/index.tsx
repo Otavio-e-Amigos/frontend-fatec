17b2514dafa4c1554cd2d54d1a5b0fb685cc3f66
DiegoG784 <diegofaria865@gmail.com>
2026-09-16 03:14:16 -0300

    visual component for showing process steps

--- app/pages/dev-area/index.tsx
+++ app/pages/dev-area/index.tsx
@@ -19,6 +19,22 @@ const defaultTableTest = new DefaultTableModel(
   ["column01", "column02", "column03"]
 )
 
+function StepBadge({step, description, final, concluded, active }: {step, description?: string, final?: boolean, concluded?: boolean, active?:boolean }) {
+	const concludedColor = concluded ? "bg-hint-success" : "bg-hint-success-inactive"
+	const activeColor = concluded ? "bg-hint-success" : active ? "bg-hint-success" : "bg-hint-success-inactive"
+	return (
+		<div className="flex flex-col">
+			<div className="flex flex-row items-center gap-2">
+				<div className={`rounded-full ${activeColor} size-15 flex items-center justify-center`}>
+					{step}
+				</div>
+				{!final && <span className={`max-h-1.5 size-15 ${concludedColor} rounded-full`} />}
+			</div>
+			<p className="overflow-hidden wrap-break-word flex-wrap w-15">{description}</p>
+		</div>
+	)
+}
+
 
 export default function Page() {
   return (
@@ -33,9 +49,11 @@ export default function Page() {
         {/*<br/>*/}
         <ComplexTableModelView data={defaultTableTest} />
 
-        <PageSection name="Components Area" />
+				<PageSection name="Components Area" />
 
-        <p className="mb-3">Buttons</p>
+
+
+				<p className="mb-3">Buttons</p>
         <section className="grid grid-cols-3 gap-3 w-fit">
           <button className="btn btn-normal">Normal button</button>
           <button className="btn btn-cancel">Cancel button</button>
@@ -54,7 +72,7 @@ export default function Page() {
         <br />
 
         <p className="mb-3">Forms</p>
-        <section className="flex flex-col gap-1 w-fit bg-gray-500 p-3">
+        <section className="flex flex-col gap-1 w-fit">
           <Form className="flex flex-col gap-2">
             <FormInput name={"textField"} label="Text Field" type={"text"} placeholder="" />
 
@@ -72,6 +90,32 @@ export default function Page() {
 
         <PageSection name={"Random Tests Area"} />
 
+        <p>as components</p>
+				<section className="flex flex-row items-start gap-2 mb-5">
+					<StepBadge step={1} description={'undefinedasdiojasiojiasdjioasdjasdiojasdiojasdjioisddjsidsoosjiosiojsiosiojd'} concluded />
+					<StepBadge step={2} description={'undefined'} active />
+					<StepBadge step={3} description={'undefined3'} final />
+				</section>
+
+				<p>as pure HTML and using grid system</p>
+				<section className="grid grid-cols-5 grid-rows-1 items-center w-fit gap-2 my-3">
+					{/*<div className="flex flex-row gap-2 items-center">*/}
+						<div className="rounded-full bg-green-500 size-15 flex items-center justify-center">
+							1
+						</div>
+						<span className="max-h-1.5 size-15 bg-green-950 rounded-full" />
+						<div className="rounded-full bg-green-500 size-15 flex items-center justify-center">
+							2
+						</div>
+						<span className="max-h-1.5 size-15 bg-green-950 rounded-full" />
+						<div className="rounded-full bg-green-500 size-15 flex items-center justify-center">
+							3
+						</div>
+						<p className="w-min overflow-hidden wrap-break-word text-wrap">descriptionopsfdokpopskdopfkdfopkfopksdfopksfdoppdposkapko</p>
+						<span />
+
+					{/*</div>*/}
+				</section>
       </section>
     </main>
   )
